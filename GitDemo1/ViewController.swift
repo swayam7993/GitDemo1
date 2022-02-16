445ce7719f18fab25d2c1b8dbe26cee04586618b
swayam7993 <swayamdipta02@gmail.com>
2022-02-16 16:43:56 +0530

    Updated code for feature1

--- GitDemo1/ViewController.swift
+++ GitDemo1/ViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
-
+import CoreLocation
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
